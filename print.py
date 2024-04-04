a062a3615f64d8c1843303a8f5aaadfc28bcf6b1
soumit02 <soumitdey2002@gmail.com>
2024-04-04 19:19:31 +0600

    Initial commit

---
+++ print.py
@@ -0,0 +1,10 @@
+print("hello world")
+a=int(input("enter a number:"))
+b=int(input("enter b number:"))
+c=int(input("enter c number:"))
+if (a>b and a>c):
+    print(a)
+elif(b>a and b>c):
+    print(b)
+else:
+   print(c)
